cd2475c1e56ad23a57352c1d99e6b2293b6b42a1
Mattie Schraeder <mathilda.schraeder@dialexa.com>
2022-12-06 09:13:34 -0600

    Day 6 part 1

---
+++ 2022/6/6.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const path = require('path');
+const filePath = path.join(__dirname, '6.txt');
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+
+    let marker = [];
+    let count = 0;
+
+    for (let c of data.split('')) {
+        marker.push(c);
+        count++;
+
+        if (marker.length > 4) {
+            marker.shift();
+        }
+
+        if (marker.length === 4) {
+            let testMarker = marker.join('');
+            let clean = true;
+
+            for (let i = 0; i < testMarker.length; i++) {
+                let split = testMarker.split(testMarker[i]);
+
+                if (split.length > 2) {
+                    clean = false;
+                    break;
+                }
+            }
+
+            if (clean) {
+                console.log(marker.join(''));
+                console.log(count);
+                break;
+            }
+        }
+    }
+});
